dd038a9324b2c3a82f8eedc687b07cd590d895db
nikoagge <nikoagge@gmail.com>
2023-09-06 14:37:58 +0300

    Initial Commit

---
+++ MapInfoAppInSwiftUI/MapInfoAppInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapInfoAppInSwiftUIApp.swift
+//  MapInfoAppInSwiftUI
+//
+//  Created by Nikos Aggelidis on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MapInfoAppInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
